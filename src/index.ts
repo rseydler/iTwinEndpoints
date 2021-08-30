2eae6badcff94d2162547ce64ce0b2f1c25e8ac2
Ron Seydler <ron.seydler@bentley.com>
2021-08-30 10:00:39 +1000

    updated fred

--- src/index.ts
+++ src/index.ts
@@ -16,7 +16,7 @@ app.post("/test", (req,res) => {
 });
 
 app.get("/fred", (req,res) => {
-  res.status(400);
+  res.status(200);
   res.json({test:"You reached fred"});
   console.log("You hit the fred endpoint");
 });
@@ -31,7 +31,7 @@ import crypto from "crypto";
 
 function validateSignature(payload: string, signatureHeader: string): boolean {
     // Replace with your own webhook secret later
-    const secret = "a505a6ba0261d02494e6ebfc9edfcc93619e55c3603f8cf47d5aaa336c2e4e32";
+    const secret = "637d07728dc5bed7a71ef407810c963ff7584be104cd26b7e3cfb428d1aef6ec";
   
     const [algorithm, signature] = signatureHeader.split("=");
     const generated_sig = crypto.createHmac(algorithm, secret).update(payload, "utf-8").digest("hex");
@@ -39,40 +39,55 @@ function validateSignature(payload: string, signatureHeader: string): boolean {
     return generated_sig.toLowerCase() === signature.toLowerCase();
   }
 
-  import { Event, NamedVersionCreatedEvent } from "./models";
+import { Event, NamedVersionCreatedEvent } from "./models";
 
-  app.post("/events", (req, res) => {
-    const signatureHeader = req.headers["signature"] as string;
-   if (!signatureHeader || !req.body){
-     res.sendStatus(401);
-     return;
-    }; //someone poking the URL maybe?
+app.post("/events", (req, res) => {
+  const signatureHeader = req.headers["signature"] as string;
+  if (!signatureHeader || !req.body){
+    res.sendStatus(401);
+    return;
+  }; //someone poking the URL maybe?
 
-    const event = JSON.parse(req.body) as Event;
-    switch (event.contentType) {
-      case "NamedVersionCreatedEvent": {
-        const content = event.content as NamedVersionCreatedEvent;
-        console.log(`New named version (ID: ${content.versionId}, Name: ${content.versionName}) was created for iModel (ID: ${content.imodelId}) ${req.body} `);
-        res.sendStatus(200);
-        break;
-      }
-      case "iModelDeletedEvent": {
-          const content = event.content as NamedVersionCreatedEvent;
-          console.log(`iModel Deleted ${req.body} (ID: ${content.imodelId})`);
-          res.sendStatus(200);
-          break;
-      }
-      case "ChangeSetPushedEvent": {
-        const content = event.content as NamedVersionCreatedEvent;
-        console.log(`New change set was pushed for ${req.body} iModel (ID: ${content.imodelId})`);
-        res.sendStatus(200);
-        break;
-      }
-      default:
-        res.sendStatus(400); //Unexpected event type
+  const event = JSON.parse(req.body) as Event;
+  switch (event.contentType) {
+    case "NamedVersionCreatedEvent": {
+      const content = event.content as NamedVersionCreatedEvent;
+      console.log(`New named version (ID: ${content.versionId}, Name: ${content.versionName}) was created for iModel (ID: ${content.imodelId}) ${req.body} `);
+      res.sendStatus(200);
+      break;
     }
-  });
+    case "iModelDeletedEvent": {
+        const content = event.content as NamedVersionCreatedEvent;
+        console.log(`iModel Deleted ${req.body} (ID: ${content.imodelId})`);
+        res.sendStatus(200);
+        break;
+    }
+    case "ChangeSetPushedEvent": {
+      const content = event.content as NamedVersionCreatedEvent;
+      console.log(`New change set was pushed for ${req.body} iModel (ID: ${content.imodelId})`);
+      res.sendStatus(200);
+      break;
+    }
+    default:
+      res.sendStatus(400); //Unexpected event type
+  }
+});
 
+function logInToBentleyAPI(){
+
+  const loginResponse = await fetch("https://ims.bentley.com/connect/token", {
+    mode: 'cors',
+    method: 'POST',
+    headers: {
+        'grant_type': 'client_credentials',
+        'client_id': 'service-uFqb5RX7yFhJFXAlp6bUI3A82',
+        'client_secret': 'zn/ZXz7OpGA53Y+UxYMxAtoXmxybW5VFo/JGsswxliVlYBm4zgMgi6necOB5c/zxTOcY7zk7o+poFn05PZPAjw==';
+        'scope': 'imodels:read projects:read connections:modify';
+      },
+    })
+  const loginData = await loginResponse;
+  const json = await loginData.json();
+}
 
   /*
 {
@@ -80,4 +95,36 @@ function validateSignature(payload: string, signatureHeader: string): boolean {
         "secret": "c7f4a7489f62ed5d70191ae2b1afbc9fcb3079f447f86949e8a2d4ff49569583"
     }
 }
+
+{
+    "imodelId": "773fd702-c1bc-4453-b6f9-02c0efd26d8f",
+    "eventTypes": [
+        "iModelDeletedEvent",
+"NamedVersionCreatedEvent",
+"ChangeSetPushedEvent"
+    ],
+    "callbackUrl": "https://imodelwebhooksample.herokuapp.com/events",
+}
+
+New change set was pushed for 
+{"content":
+  {"briefcaseId":"2",
+  "changesetId":"4e65fa6dbd182527f358c509ede3dad3948004a0",
+  "changesetIndex":"15",
+  "imodelId":"773fd702-c1bc-4453-b6f9-02c0efd26d8f",
+  "projectId":"2ad52e0d-3ce7-4177-ae71-a2bea7da31df"}
+,"contentType":"ChangeSetPushedEvent",
+"enqueuedDateTime":"Fri, 27 Aug 2021 02:23:49 GMT",
+"messageId":"43d0fd2e53944fde9cfe190abc50a41e",
+"subscriptionId":"4f49f011-33f3-46d4-b5ca-7f84bd0a31e0"}
+ iModel (ID: 773fd702-c1bc-4453-b6f9-02c0efd26d8f)
+
+
+//client secret for app 
+
+service-uFqb5RX7yFhJFXAlp6bUI3A82
+0nkAGHJ1jiAfbkCUvyrdewN1PkPaLcNDsODltDtVC5DvtUbuGHyqkk2aRIZsnLZMKNjm2yG/TT5Fa3V0CNuDIw==
+service-uFqb5RX7yFhJFXAlp6bUI3A82@apps.imsoidc.bentley.com
+
+
 */
